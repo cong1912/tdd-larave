18b0187dfd29a659011c5166cb291f48680f780c
unknown <vothithuy1507@gmail.com>
2020-07-19 17:16:58 +0700

    xong video 25

--- app/Task.php
+++ app/Task.php
@@ -18,12 +18,12 @@ class Task extends Model
 
     public  function complete(){
         $this->update(['completed'=> true]);
-        $this->project->recordActivity('completed_task');
+        $this->recordActivity('completed_task');
 
     }
     public  function incomplete(){
         $this->update(['completed'=> false]);
-        $this->project->recordActivity('incompleted_task');
+        $this->recordActivity('incompleted_task');
 
     }
     public function project(){
@@ -34,4 +34,16 @@ class Task extends Model
    public function path(){
        return "/projects/{$this->project->id}/tasks/{$this->id}";
    }
+
+
+    public function recordActivity($description){
+        $this->activity()->create([
+            'project_id'=>$this->project_id,
+            'description'=>$description
+        ]);
+
+    }
+    public function activity(){
+        return   $this->morphMany(Activity::class, 'subject')->latest();
+    }
 }
--- tests/Feature/TriggerActivityTest.php
+++ tests/Feature/TriggerActivityTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Facades\Tests\Setup\ProjectFactory;
@@ -32,7 +33,16 @@ class TriggerActivityTest extends TestCase
         $project = ProjectFactory::create();
         $project->addTask('Some task');
         $this->assertCount(2,$project->activity);
-        $this->assertEquals('created_task',$project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity){
+
+            $this->assertEquals('created_task',$activity->description);
+
+            $this->assertInstanceOf(Task::class, $activity->subject);
+
+            $this->assertEquals('Some task',$activity->subject->body);
+
+        });
 
     }
     /** @test*/
@@ -44,7 +54,14 @@ class TriggerActivityTest extends TestCase
            'completed'=>true
         ]);
         $this->assertCount(3,$project->activity);
-        $this->assertEquals('completed_task',$project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity){
+
+            $this->assertEquals('completed_task',$activity->description);
+
+            $this->assertInstanceOf(Task::class, $activity->subject);
+
+        });
 
     }
     /** @test*/
